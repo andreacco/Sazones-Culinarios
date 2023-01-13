72034099a60f504ccf2a0d95bd731327659bf5cb
Andrea Carvajal Osorio <andreacc9199@gmail.com>
2023-01-13 05:00:16 -0400

    advances

--- client/src/components/Home/BestSellers/BestSellers.tsx
+++ client/src/components/Home/BestSellers/BestSellers.tsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import '../../../scss/components/BestSellers.scss'
 
 export default function BestSellers() {
   
   return (
-    <div>
+    <div className='BestSellers-container'>
       <h2>BestSellers</h2>
-      <div>
-        <img src="" alt="" />
-        <h1></h1>
+      <div className='cards-container'>
+        <div className="flip-card">
+          <div className="flip-card-inner">
+              <div className="flip-card-front">
+              </div>
+              <div className="flip-card-back">
+                  <p className="title">Bartender Profesional</p>
+                  <p>Haz click para más información</p>
+              </div>
+          </div>
+        </div>
+        <div className="card">
+            <div className="card-details">
+              <p className="text-title">Bartender Profesional</p>
+              <p className="text-body">La carrera del futuro</p>
+            </div>
+            <button className="card-button">Haz click para más información</button>
+        </div>
+        <div className="container">
+            <div className="card_box">
+                <span></span>
+            </div>
+        </div>
       </div>
     </div>
   )
--- client/src/components/ProductCard/ProductCard.tsx
+++ client/src/components/ProductCard/ProductCard.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { useSelector } from "react-redux"
 import { getAllProducts } from "../../redux/actions"
+import '../../scss/components/Products.scss'
 
 function ProductCard() {
     const dispatch: any = useDispatch()
@@ -11,13 +12,29 @@ function ProductCard() {
     }, [dispatch])
 
     const productsArr = useSelector((state: any) => state.products)
-    
+
+    const prod = productsArr[0].description.substring(0,90)
+    console.log(`${prod}...`)
     return (
-        <div>
+        <div className="products-container">
             {productsArr?.map((p: any) => {
                 return (
-                    <div key={p._id}>
-                        <img src={p.cover_image} alt="cover-img" height={200}/>
+                    <div key={p._id} className="card">
+                        <div className="header">
+                            <div className="img-box">
+                                <img src={p.cover_image} alt="cover-img" height={200}/>
+                            </div>
+                            <h1 className="title">{p.name}</h1>
+                        </div>
+
+                        <div className="content">
+                            <p>
+                                {(p.description).substring(0,90)}...
+                            </p>
+                            <button className="boton btn-link">
+                                <span>Más Información</span>
+                            </button>  
+                        </div>
                     </div>
                 )
             })}
