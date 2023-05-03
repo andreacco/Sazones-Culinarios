b986ca4dedac2e048b003301c6b6050f92b67c69
Andrea Carvajal Osorio <andreacc9199@gmail.com>
2023-05-03 16:50:28 -0400

    cors 2

--- Servers/src/app.js
+++ Servers/src/app.js
@@ -19,19 +19,20 @@ server.use(body_parser_1.default.urlencoded({ extended: true, limit: '50mb' }));
 server.use(body_parser_1.default.json({ limit: '50mb' }));
 server.use((0, cookie_parser_1.default)());
 server.use((0, morgan_1.default)('dev'));
-// server.use((_req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');  
-//   res.header('Access-Control-Allow-Credentials', 'false');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'false');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 //--------------------------------------------------//
 server.use('/api', index_1.default);
-server.use((0, cors_1.default)({
-    credentials: false,
-    origin: '*',
-}));
+server.use((0, cors_1.default)());
+// server.use(cors({
+//   credentials: false,
+//   origin: '*',
+// }));
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 server.use((err, _req, res, _next) => {
     const status = err.status || 500;
--- Servers/src/app.ts
+++ Servers/src/app.ts
@@ -15,20 +15,21 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-// server.use((_req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');  
-//   res.header('Access-Control-Allow-Credentials', 'false');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');  
+  res.header('Access-Control-Allow-Credentials', 'false');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 //--------------------------------------------------//
 
 server.use('/api', routes);
-server.use(cors({
-  credentials: false,
-  origin: '*',
-}));
+server.use(cors())
+// server.use(cors({
+//   credentials: false,
+//   origin: '*',
+// }));
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 server.use((err:any, _req:any, res:any, _next:any) => {
