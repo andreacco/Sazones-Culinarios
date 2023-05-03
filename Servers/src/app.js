f8961e6167534733d9e20efc60f1b19e1a8d63db
Andrea Carvajal Osorio <andreacc9199@gmail.com>
2023-05-03 16:01:06 -0400

    probando solucion cors

--- Servers/src/app.js
+++ Servers/src/app.js
@@ -7,28 +7,27 @@ exports.server = void 0;
 const express_1 = __importDefault(require("express"));
 const index_1 = __importDefault(require("./routes/index"));
 // import passport from 'passport';
-// import cookieParser from 'cookie-parser';
-// import bodyParser from 'body-parser';
-// import morgan from 'morgan';
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const morgan_1 = __importDefault(require("morgan"));
 require("./config/MongoDb/Db");
-const cors_1 = __importDefault(require("cors"));
 const server = (0, express_1.default)();
 exports.server = server;
 //-------------------cors config--------------------//
-// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-// server.use(bodyParser.json({ limit: '50mb' }));
-// server.use(cookieParser());
-// server.use(morgan('dev'));
-// server.use((_req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');  
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use(body_parser_1.default.urlencoded({ extended: true, limit: '50mb' }));
+server.use(body_parser_1.default.json({ limit: '50mb' }));
+server.use((0, cookie_parser_1.default)());
+server.use((0, morgan_1.default)('dev'));
+server.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 //--------------------------------------------------//
 server.use('/api', index_1.default);
-server.use((0, cors_1.default)());
+// server.use(cors());
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 server.use((err, _req, res, _next) => {
     const status = err.status || 500;
--- Servers/src/app.ts
+++ Servers/src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import routes from './routes/index';
 // import passport from 'passport';
-// import cookieParser from 'cookie-parser';
-// import bodyParser from 'body-parser';
-// import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 import './config/MongoDb/Db';
 import cors from 'cors';
 
@@ -11,21 +11,21 @@ const server = express();
 
 
 //-------------------cors config--------------------//
-// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-// server.use(bodyParser.json({ limit: '50mb' }));
-// server.use(cookieParser());
-// server.use(morgan('dev'));
-// server.use((_req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');  
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');  
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 //--------------------------------------------------//
 
 server.use('/api', routes);
-server.use(cors());
+// server.use(cors());
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 server.use((err:any, _req:any, res:any, _next:any) => {
